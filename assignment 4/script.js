899980bdba0cce60b0470ec7eb702976aad01db0
Abhiram-hub <79072197+Abhiram-hub@users.noreply.github.com>
2021-03-31 19:25:32 +0530

    Update script.js

--- assignment 4/script.js
+++ assignment 4/script.js
@@ -1,14 +1,14 @@
 var names=new Array();
-names[0]="yakoov";
-names[1]="jhon";
+names[0]="Yaakov";
+names[1]="John";
 names[2]="Jen";
 names[3]="jason";
 names[4]="paul";
 names[5]="frank";
 names[6]="larry";
-names[7]="parul";
-names[8]="jin";
-names[9]="mohith";
+names[7]="paula";
+names[8]="laura";
+names[9]="jim";
 
 
 for (var i = 0; i< names.length; i++){
